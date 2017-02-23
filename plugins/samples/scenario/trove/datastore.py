5390f0897c862c2eec54b50885a91ad02fc97962
VoidBird <1918601366@qq.com>
2017-02-23 18:18:33 +0800

    Trove datastore plugins and tasks

--- plugins/samples/scenario/trove/datastore.py
+++ plugins/samples/scenario/trove/datastore.py
@@ -6,16 +6,16 @@ from rally.task import atomic
 from pdb import set_trace
 
 
-@scenario.configure(name="EayunTrove.list_datastore")
+@scenario.configure(name="EayunTrove.list_datastores")
 class ListDatastore(scenario.OpenStackScenario):
 
-    @atomic.action_timer("list_datastore")
+    @atomic.action_timer("list_datastores")
     def _list_all_datastores(self):
         self.clients("trove").datastores.list()
 
-    @atomic.action_timer("list_datastore_as_admin")
+    @atomic.action_timer("list_datastores_as_admin")
     def _list_all_datastores_as_admin(self):
-        self.admin_client("trove").datastores.list()
+        self.admin_clients("trove").datastores.list()
 
     def run(self):
         self._list_all_datastores()
@@ -28,13 +28,14 @@ class ShowDatastore(scenario.OpenStackScenario):
     @atomic.action_timer("show_datastore")
     def _show_datastore(self):
         client = self.clients("trove")
-        set_trace()
-        client.datastores.get(client.datastores.list()[0])
+        datastore = client.datastores.list()[0]
+        client.datastores.get(datastore)
 
     @atomic.action_timer("show_datastore_as_admin")
     def _show_datastore_as_admin(self):
         client = self.admin_clients("trove")
-        client.datastores.get(client.datastores.list()[0])
+        datastore = client.datastores.list()[0]
+        client.datastores.get(datastore)
 
     def run(self):
         self._show_datastore()
@@ -48,15 +49,36 @@ class ListDatastoreVersion(scenario.OpenStackScenario):
     def _list_datastore_version(self):
         client = self.clients("trove")
         datastore = client.datastores.list()[0]
-        set_trace()
-        client.datastore_versions.list(datastore)
+        client.datastore_versions.list(datastore.id)
 
-    @atomic.action_timer("list_datastore_as_admin")
-    def _list_all_datastores_as_admin(self):
+    @atomic.action_timer("list_datastore_version_as_admin")
+    def _list_datastore_version_as_admin(self):
         client = self.admin_clients("trove")
         datastore = client.datastores.list()[0]
-        client.datastore_versions.list(datastore)
+        client.datastore_versions.list(datastore.id)
 
     def run(self):
-        self._list_all_datastores()
-        self._list_all_datastores_as_admin()
+        self._list_datastore_version()
+        self._list_datastore_version_as_admin()
+
+
+@scenario.configure(name="EayunTrove.show_datastore_version")
+class ShowDatastoreVersion(scenario.OpenStackScenario):
+
+    @atomic.action_timer("show_datastore_version")
+    def _show_datastore_version(self):
+        client = self.clients("trove")
+        datastore = client.datastores.list()[0]
+        datastore_version = client.datastore_versions.list(datastore.id)[0]
+        client.datastore_versions.get_by_uuid(datastore_version.id)
+
+    @atomic.action_timer("show_datastore_version_as_admin")
+    def _show_datastore_version_as_admin(self):
+        client = self.admin_clients("trove")
+        datastore = client.datastores.list()[0]
+        datastore_version = client.datastore_versions.list(datastore.id)[0]
+        client.datastore_versions.get_by_uuid(datastore_version.id)
+
+    def run(self):
+        self._show_datastore_version()
+        self._show_datastore_version_as_admin()
